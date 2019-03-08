efd8763b1f717b4782ee54d36e9824a16625f5bb
Junenatte-and-little-pig <sy5622_5@126.com>
2019-03-08 16:15:55 +0800

    leetcode day 1

---
+++ leetcode/src/283.移动零.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=java
+ *
+ * [283] 移动零
+ *
+ * https://leetcode-cn.com/problems/move-zeroes/description/
+ *
+ * algorithms
+ * Easy (52.78%)
+ * Total Accepted:    35.6K
+ * Total Submissions: 67.4K
+ * Testcase Example:  '[0,1,0,3,12]'
+ *
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ * 
+ * 示例:
+ * 
+ * 输入: [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ * 
+ * 说明:
+ * 
+ * 
+ * 必须在原数组上操作，不能拷贝额外的数组。
+ * 尽量减少操作次数。
+ * 
+ * 
+ */
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int cnt=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0)
+                {nums[cnt]=nums[i];
+                cnt++;
+        }
+    }
+        for(;cnt<nums.length;cnt++)
+            nums[cnt]=0;
+    }
+}
+
---
+++ leetcode/src/409.最长回文串.java
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=409 lang=java
+ *
+ * [409] 最长回文串
+ *
+ * https://leetcode-cn.com/problems/longest-palindrome/description/
+ *
+ * algorithms
+ * Easy (46.56%)
+ * Total Accepted:    3.8K
+ * Total Submissions: 8.1K
+ * Testcase Example:  '"abccccdd"'
+ *
+ * 给定一个包含大写字母和小写字母的字符串，找到通过这些字母构造成的最长的回文串。
+ * 
+ * 在构造过程中，请注意区分大小写。比如 "Aa" 不能当做一个回文字符串。
+ * 
+ * 注意:
+ * 假设字符串的长度不会超过 1010。
+ * 
+ * 示例 1: 
+ * 
+ * 
+ * 输入:
+ * "abccccdd"
+ * 
+ * 输出:
+ * 7
+ * 
+ * 解释:
+ * 我们可以构造的最长的回文串是"dccaccd", 它的长度是 7。
+ * 
+ * 
+ */
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] cnts=new int[58];
+        int result=0;
+        boolean hasSingle=false;
+        for(char c:s.toCharArray())
+            cnts[c-'A']++;
+        for(int cnt:cnts)
+        {
+            if(cnt%2==1&&!hasSingle)
+                hasSingle=true;
+            result+=cnt-cnt%2;
+        }
+        if(hasSingle)
+            result+=1;
+        return result;
+    }
+}
+
