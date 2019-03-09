0ef7abd15f883f9553adb2d476a451669bbcf966
Junenatte-and-little-pig <sy5622_5@126.com>
2019-03-09 19:30:37 +0800

    leetcode day 2 end

--- leetcode/src/605.种花问题.java
+++ leetcode/src/605.种花问题.java
@@ -41,6 +41,33 @@
  */
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-
+        if (n > flowerbed.length)
+            return false;
+        if (flowerbed.length == 1)
+            if ((flowerbed[0] == 0 && n <= 1)||(flowerbed[0]==1&&n==0))
+                return true;
+            else
+                return false;
+        int cnt = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0) {
+                if (i - 1 < 0)
+                    if (flowerbed[i + 1] == 0) {
+                        cnt++;
+                        flowerbed[i] = 1;
+                    }
+                if (i - 1 >= 0 && i + 1 < flowerbed.length)
+                    if (flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0) {
+                        cnt++;
+                        flowerbed[i] = 1;
+                    }
+                if (i + 1 >= flowerbed.length)
+                    if (flowerbed[i - 1] == 0) {
+                        cnt++;
+                        flowerbed[i] = 1;
+                    }
+            }
+        }
+        return n <= cnt;
     }
 }
